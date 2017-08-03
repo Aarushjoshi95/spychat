46e1204d1472355bcda9a502176c5ea9cf4e9e12
Aarush Joshi <aarushjoshi123@gmail.com>
2017-08-03 13:48:21 +0530

    spyidentity created

---
+++ spychat.py
@@ -0,0 +1,5 @@
+print("hey new to python")
+spy_name = raw_input("provide ur user name: ")
+spy_slautation = raw_input("what should we call u ? :")
+spy_name = spy_slautation + ' ' + spy_name
+print ('welcome ' + spy_name + ' Glad to have u back ? : ')
