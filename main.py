e0f00fd9f2032d0adbe97fb3ce6bba60dd2f35b3
Aarushjoshi95 <30682500+Aarushjoshi95@users.noreply.github.com>
2017-09-21 07:31:50 -0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-print("hey new to python")
-spy_name = raw_input("provide ur user name: ")
-spy_slautation = raw_input("what should we call u ? :")
-
-if len(spy_name) > 0:
-    print ('alright' + spy_name + 'ike to know little more about u before we procesed ')
-spy_name=spy_slautation +  " " + spy_name
-    print ('welcome {0} Glad to have u back ?'.(spy_name)
